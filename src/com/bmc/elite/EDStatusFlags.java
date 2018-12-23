8f6990684f5da9cd31e40af15286ef73453ce635
Arturs Ziborovs <arturs.ziborovs@gmail.com>
2018-12-23 19:17:31 +0200

    - added more flag and gui mode information from http://hosting.zaonce.net/community/journal/v18/Journal_Manual_v18.pdf

--- src/com/bmc/elite/EDStatusFlags.java
+++ src/com/bmc/elite/EDStatusFlags.java
@@ -3,12 +3,34 @@ package com.bmc.elite;
 import java.util.HashMap;
 
 public class EDStatusFlags {
-    public static final int HUD_DISCOVERY_MODE = 1 << 27;
-    public static final int LANDING_GEAR = 1 << 2;
-    public static final int SUPERCRUISE = 1 << 4;
-    public static final int HARDPOINTS = 1 << 6;
-    public static final int SHIP_LIGHTS = 1 << 8;
-    public static final int CARGO_SCOOP = 1 << 9;
+    public static final int DOCKED                          = 1;
+    public static final int LANDED_PLANET                   = 1 << 1;
+    public static final int LANDING_GEAR                    = 1 << 2;
+    public static final int SHIELDS_UP                      = 1 << 3;
+    public static final int SUPERCRUISE                     = 1 << 4;
+    public static final int FA_OFF                          = 1 << 5;
+    public static final int HARDPOINTS                      = 1 << 6;
+    public static final int IN_WING                         = 1 << 7;
+    public static final int SHIP_LIGHTS                     = 1 << 8;
+    public static final int CARGO_SCOOP                     = 1 << 9;
+    public static final int SILENT_RUNNING                  = 1 << 10;
+    public static final int SCOOPING                        = 1 << 11;
+    public static final int SRV_HANDBRAKE                   = 1 << 12;
+    public static final int SRV_TURRET                      = 1 << 13;
+    public static final int SRV_UNDER_SHIP                  = 1 << 14;
+    public static final int SRV_DRIVE_ASSIST                = 1 << 15;
+    public static final int MASS_LOCK                       = 1 << 16;
+    public static final int FSD_CHARGING                    = 1 << 17;
+    public static final int FSD_COOLDOWN                    = 1 << 18;
+    public static final int LOW_FUEL                        = 1 << 19; // Less than 25%
+    public static final int OVERHEATING                     = 1 << 20; // More than 100%
+    public static final int HAS_LAT_LONG                    = 1 << 21;
+    public static final int IN_DANGER                       = 1 << 22;
+    public static final int INTERDICTION                    = 1 << 23;
+    public static final int IN_SHIP                         = 1 << 24;
+    public static final int IN_FIGHTER                      = 1 << 25;
+    public static final int IN_SRV                          = 1 << 26;
+    public static final int HUD_DISCOVERY_MODE              = 1 << 27;
 
     public static final HashMap<Integer, String> STATUS_TO_CONTROL = new HashMap<Integer, String>() {
         {
